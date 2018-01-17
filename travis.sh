ba4bcd4aa85dd6ff96c16322dce15dfddcfd2284
adelegue <adelegue@hotmail.com>
2018-01-17 11:06:15 +0100

    Snapshot build

--- travis.sh
+++ travis.sh
@@ -2,11 +2,6 @@
 
 if test "$TRAVIS_PULL_REQUEST" = "false"
 then
-    if [ -z "$TRAVIS_TAG" ];
-    then
-      sbt ++$TRAVIS_SCALA_VERSION ';test'
-    else
-      sbt ++$TRAVIS_SCALA_VERSION ";test;publish"
-    fi
+    sbt ++$TRAVIS_SCALA_VERSION ";test;publish"
 fi
 
